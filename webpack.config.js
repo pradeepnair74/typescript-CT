5dc2cdf70e2299350892dcca6162b263b38fc4b4
John Doe <johndoe@example.com>
2022-03-29 23:23:52 +0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ]
+  },
+  mode: 'development',
+  target: 'node',
+};
+
+module.exports = config;
